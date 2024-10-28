5762d7f4f38bb225b345e3f9809d83645c755f4e
AxelSalim <adjakidjememiaghe@gmail.com>
2024-10-28 12:53:49 +0100

    Exporter un chargeur à partir de root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,12 @@
 import { Outlet, Link } from "react-router-dom";
+import { getContacts } from "../contacts";
 
+
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
 
 export default function Root() {
   return (
